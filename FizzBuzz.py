d2b3ec71a3a6ddfaac20af17864b8324a9b6a8db
Student-Ayman <aymanbrahimi95@gmail.com>
2023-10-02 16:16:57 +0200

    premier commit

---
+++ FizzBuzz.py
@@ -0,0 +1,7 @@
+class FizzBuzz:
+    def affiche(self,nombre):
+        resultat=""
+        
+        for i in 
+    
+        return resultat
---
+++ Test_FizzBuzz.py
@@ -0,0 +1,10 @@
+import unittest
+from FizzBuzz import *
+
+class Test_FizzBuzz(unittest.TestCase):
+
+    def setUp(self):
+        self.instance=FizzBuzz()
+
+    
+
